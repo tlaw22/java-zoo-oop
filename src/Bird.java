c93454a7572207e70027060825969f9f223e3394
tlaw22 <timothy.j.lawless@youtlook.com>
2023-12-29 10:30:36 -0500

    Extended animal class

--- src/Bird.java
+++ src/Bird.java
@@ -1,4 +1,9 @@
-public class Bird {
+public class Bird extends Animal{
+    public Bird(int age, String gender, int weight) {
+        super(age, gender, weight);
+        
+    }
+
     public void fly(){
         System.out.println("Flying...");
     }
--- src/Fish.java
+++ src/Fish.java
@@ -1,4 +1,10 @@
-public class Fish {
+public class Fish extends Animal {
+
+    public Fish(int age, String gender, int weight) {
+        super(age, gender, weight);
+        
+    }
+
     public void swim(){
         System.out.println("Swimming...");
     }
